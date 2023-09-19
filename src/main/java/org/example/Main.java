75e35fd742876e536a9e8832245ba7e172fa988f
1 <glotov-aleks@mail.ru>
2023-09-19 15:10:15 +0400

    a

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,31 @@
 package org.example;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int sizeArr = 100_000;
+        IntegerList integerList1 = new IntegerListImpl(100_000);
+        Random r = new Random();
+        for (int i = 0; i < sizeArr; i++) {
+            integerList1.add(r.nextInt(1_000_000) + 1);
+        }
+        System.out.println("stringList = " + integerList1);
+        System.out.println();
+        integerList1.add(58);
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+
+        IntegerList integerList2 = new IntegerListImpl();
+        integerList1.integerListCopy(integerList2);
+        long start1 = System.currentTimeMillis();
+        System.out.println(integerList1.contains(56, "QuickSort"));
+        System.out.println(System.currentTimeMillis() - start1);
+        long start2 = System.currentTimeMillis();
+        System.out.println(integerList1.contains(56, "MergeSort"));
+        System.out.println(System.currentTimeMillis() - start2);
     }
 }
